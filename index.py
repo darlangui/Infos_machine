047e5bbb8e12c75a7b272c1400079e5f0c0845cf
Darlan <darlaneduardoguimaraes@gmail.com>
2023-04-01 18:05:48 -0300

    resolution and language

--- index.py
+++ index.py
@@ -1,10 +1,8 @@
-from flask import Flask, render_template, request
-import getpass
+from flask import Flask, render_template, request, jsonify
 import json
-import os
-import platform
-import psutil
 import requests
+
+
 
 app = Flask(__name__)
 
@@ -22,33 +20,34 @@ def info(result):
     print(f"ISP :", {result['isp']})
     print(f"Organização :", {result['org']})
 
-def info_system():
-    versao_sistema = platform.version()
-    arquitetura = platform.architecture()
-    processador = platform.processor()
-    cpu_percent = psutil.cpu_percent()
-    mem = psutil.virtual_memory()
-    total_mem = round(mem.total / (1024 * 1024 * 1024), 2)
-    available_mem = round(mem.available / (1024 * 1024 * 1024), 2)
-    used_mem = round(mem.used / (1024 * 1024 * 1024), 2)
-    print(f"Nome do sistema operacional: {platform.system()}")
-    print(f"Versão do sistema operacional: {versao_sistema}")
-    print(f"Versão do sistema operacional: {platform.release()}")
-    print(f"Arquitetura da máquina: {platform.machine()}")
-    print(f"Arquitetura: {arquitetura}")
-    print(f"Processador: {processador}")
-    print(f"Uso de CPU: {cpu_percent}%")
-    print(f"Memória total: {total_mem} GB")
-    print(f"Memória disponível: {available_mem} GB")
-    print(f"Memória usada: {used_mem} GB")
-    print(f"Nome do usuário: ", {getpass.getuser()})
-@app.route('/')
-def index():  # put application's code here
-    info(search_ip(request.remote_addr))
-    print('\n\n------------------------------------------------\n\n')
-    info_system()
+@app.route('/get_resolution', methods=['POST'])
+def get_resolution_data():
+    width = request.form.get('width')
+    height = request.form.get('height')
+    resolution = {'width': width, 'height': height}
+    print(f"Screen Resolution: {width} x {height}")
+    return "OK"
+
+@app.route('/get_resolution')
+def get_resolution():
     return render_template('index.html')
 
+@app.route('/')
+def index():  # put application's code here
+    '''info(search_ip(request.remote_addr))'''
+    print('\n\n------------------------------------------------\n\n')
+    print(request.headers.get('User-Agent'))
+    print('\n\n------------------------------------------------\n\n')
+    print(request.referrer)
+    print('\n\n------------------------------------------------\n\n')
+    print(request.method)
+    print('\n\n------------------------------------------------\n\n')
+    print(request.url)
+    print('\n\n------------------------------------------------\n\n')
+    print(request.headers)
+    print('\n\n------------------------------------------------\n\n')
+    print(request.accept_languages)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
